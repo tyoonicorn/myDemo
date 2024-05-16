78eb8dae846de0f340c2d9d45b1d85a8a76aa3f1
Tyler Yoon <tyleryoon@outlook.com>
2024-05-15 20:01:20 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  const handleClick = () => {
+    alert(
+      "WHAT HAVE YOU DONE, YOU DECLARED WAR ON CHINA!!! You've doomed us all; your recklessness has condemned us for 1000 years of nuclear winter!!! Our species as we know it will go extinct. We will have to take to the ground like we're in the Fallout series. Are you that big of a fan of the show???"
+    );
+  };
+
+  return (
+    <div>
+      <h1>Tyler was here</h1>;
+      <button onClick={handleClick}>pls do not press teehee :p</button>
+    </div>
+  );
+}
+
+export default App;
